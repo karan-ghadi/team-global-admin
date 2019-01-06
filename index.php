68f37420b5d316a66f1e016c63aced062bff37da
karan-ghadi <karan10071@gmail.com>
2019-01-06 23:03:55 +0530

    added temp images link

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+
+<link href="./horizontal-nav-fullwidth/dist/css/style.css" rel="stylesheet">
 <?php include_once("horizontal-nav-fullwidth/starter-kit.html"); ?>
+<script src="./assets/node_modules/jquery/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="./assets/node_modules/popper/popper.min.js"></script>
+<script src="./assets/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- slimscrollbar scrollbar JavaScript -->
+<script src="./horizontal-nav-fullwidth/dist/js/perfect-scrollbar.jquery.min.js"></script>
+<!--Wave Effects -->
+<script src="./horizontal-nav-fullwidth/dist/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="./horizontal-nav-fullwidth/dist/js/sidebarmenu.js"></script>
+<!--stickey kit -->
+<script src="./assets/node_modules/sticky-kit-master/dist/sticky-kit.min.js"></script>
+<script src="./assets/node_modules/sparkline/jquery.sparkline.min.js"></script>
+<!--Custom JavaScript -->
+<script src="./horizontal-nav-fullwidth/dist/js/custom.js"></script>
